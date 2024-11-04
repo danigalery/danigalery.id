713c60c6b5b6825fd71416d5a64e8a63ba514618
danigalery <khaidarrofiqii@gmail.com>
2024-11-05 04:23:17 +0700

    Add files via upload

---
+++ dani.js
@@ -0,0 +1,6 @@
+
+// Contact form submission (sample)
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Pesan Anda telah terkirim! Terima kasih telah menghubungi kami.');
+});
